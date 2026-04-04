faaf6e359c86cd56323cda047ce417069878da69
BerdanG <berdangencer@yahoo.com>
2026-04-04 21:22:52 +0300

    feat: implement danger zone trigger and HUD warning

---
+++ Assets/Scripts/DangerZoneController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class DangerZoneController : MonoBehaviour
+{
+    [SerializeField] private FlightExamManager examManager;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {examManager.EnterDangerZone();}
+    }
+
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {examManager.ExitDangerZone();}
+    }
+}
