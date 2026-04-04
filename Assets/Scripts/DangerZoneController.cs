dee9c3a8e913907857a8a74a20fbc9662e375a6e
BerdanG <berdangencer@yahoo.com>
2026-04-04 21:41:46 +0300

    feat: add countdown system with coroutine and cancel on exit

--- Assets/Scripts/DangerZoneController.cs
+++ Assets/Scripts/DangerZoneController.cs
@@ -1,20 +1,41 @@
 using UnityEngine;
+using System.Collections;
 
 public class DangerZoneController : MonoBehaviour
 {
     [SerializeField] private FlightExamManager examManager;
+    [SerializeField] private float missileDelay = 5f;
+
+    private Coroutine countdownCoroutine;
 
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
-        {examManager.EnterDangerZone();}
+        {
+            examManager.EnterDangerZone();
+            countdownCoroutine = StartCoroutine(StartCountdown());
+        }
     }
 
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
-        {examManager.ExitDangerZone();}
+        {
+            examManager.ExitDangerZone();
+            if (countdownCoroutine != null)
+            {
+                StopCoroutine(countdownCoroutine);
+                countdownCoroutine = null;
+            }
+        }
+    }
+
+
+    private IEnumerator StartCountdown()
+    {
+        yield return new WaitForSeconds(missileDelay);
+        Debug.Log("MISSILE SHOULD SPAWN NOW");
     }
 }
